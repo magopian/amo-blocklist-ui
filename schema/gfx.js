23c30f025a09d6b2e3f0d2f46019387c712ea0bd
Nicolas Perriault <nperriault@gmail.com>
2015-11-02 21:58:28 +0100

    Moved json schemas to their own folder.

---
+++ schema/gfx.js
@@ -0,0 +1,49 @@
+export const gfxSchema = {
+  "title": "Gfx",
+  "type": "object",
+  "properties": {
+    "os": {
+      "type": "string",
+      "description": "OS"
+    },
+    "vendor": {
+      "type": "string",
+      "description": "Vendor"
+    },
+    "devices": {
+      "type": "array",
+      "description": "Devices",
+      "items": {
+        "type": "string"
+      }
+    },
+    "feature": {
+      "type": "string",
+      "description": "Feature"
+    },
+    "featureStatus": {
+      "type": "string",
+      "description": "Feature status",
+      "enum": [
+        "",
+        "BLOCKED_DRIVER_VERSION",
+        "BLOCKED_DEVICE"
+      ]
+    },
+    "driverVersion": {
+      "type": "string",
+      "description": "Driver version"
+    },
+    "driverVersionComparator": {
+      "type": "string",
+      "description": "Driver version comparator",
+      "enum": [
+        "EQUAL",
+        "LESS_THAN",
+        "LESS_THAN_OR_EQUAL",
+        "GREATER_THAN",
+        "GREATER_THAN_OR_EQUAL"
+      ]
+    }
+  }
+};
