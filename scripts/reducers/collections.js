7883251baef5569da50e684c2b7afeaea96930f1
Nicolas Perriault <nperriault@gmail.com>
2015-12-02 16:32:54 +0100

    Extracted collections config into a JSON file.

--- scripts/reducers/collections.js
+++ scripts/reducers/collections.js
@@ -1,39 +1,5 @@
+import INITIAL_STATE from "../../config/kwac-config.json";
 import { COLLECTION_SYNCED } from "../actions/collections";
-
-/**
- * For now this list is static but later on we'll add collections and their
- * config dynamically.
- */
-const INITIAL_STATE = {
-  addons: {
-    name: "addons",
-    synced: true,
-    config: {
-      displayFields: ["enabled", "addonId"],
-    },
-  },
-  certificates: {
-    name: "certificates",
-    synced: true,
-    config: {
-      displayFields: ["enabled", "issuerName"],
-    },
-  },
-  gfx: {
-    name: "gfx",
-    synced: true,
-    config: {
-      displayFields: ["enabled", "os", "vendor", "feature"],
-    },
-  },
-  plugins: {
-    name: "plugins",
-    synced: true,
-    config: {
-      displayFields: ["enabled", "matchName", "matchFilename", "matchDescription"],
-    },
-  },
-};
 
 export default function collections(state = INITIAL_STATE, action) {
   switch(action.type) {
